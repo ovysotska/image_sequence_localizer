2dbc8bde38081a803628ff2fb0eb87f5000603ab
olga <olga.vysotska@uni-bonn.de>
2022-10-15 10:12:08 +0200

    not working test added

--- src/test/successor_manager_test.cpp
+++ src/test/successor_manager_test.cpp
@@ -1,3 +1,8 @@
+#include "successor_manager/successor_manager.h"
 #include "gtest/gtest.h"
 
+TEST(SuccessorManager, create) {
+  ASSERT_DEATH(SuccessorManager(nullptr, nullptr, /*fanOut=*/0),
+               "Database is not set.");
+}
 TEST(SuperTestName, testName) { EXPECT_TRUE(true); }
