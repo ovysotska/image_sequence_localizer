e486576743653e61cd884cfcca56550785ba1266
olga <olga.vysotska@uni-bonn.de>
2023-10-09 11:54:40 +0200

    first working version ready. Needs styling + user friendly outputs

--- src/viewer/src/App.tsx
+++ src/viewer/src/App.tsx
@@ -44,6 +44,13 @@ function App() {
     );
   }, [zoomParams, costMatrix]);
 
+  useEffect(() => {
+    console.log(
+      "Selected Cost Matrix Element changed to",
+      selectedCostMatrixElement
+    );
+  }, [selectedCostMatrixElement]);
+
   return (
     <div className="App">
       <div
