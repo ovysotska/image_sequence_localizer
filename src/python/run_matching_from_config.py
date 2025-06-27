8cac33171083a87bcb0b5ddb14c37381fc71e1a2
olga <olga.vysotska@uni-bonn.de>
2025-06-27 15:56:41 +0200

    added new run scripts

---
+++ src/python/run_matching_from_config.py
@@ -0,0 +1,37 @@
+import matching_scripts as matching
+
+import yaml
+
+import argparse
+
+
+def parseParams():
+    parser = argparse.ArgumentParser(
+        description="Run image matching starting from config yaml file."
+    )
+
+    parser.add_argument(
+        "--yaml_config_file",
+        type=str,
+        required=True,
+        help="The yaml config file.",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parseParams()
+
+    with open(args.yaml_config_file, "r") as file:
+        config = yaml.load(file, Loader=yaml.SafeLoader)
+
+    run_params = matching.RunParameters()
+    run_params = matching.initializeFromDict(run_params, config)
+
+    matching.computeCostMatrix(run_params)
+    matching.runMatching(args.yaml_config_file)
+    matching.runLocalizationResultVisualization(run_params)
+
+
+if __name__ == "__main__":
+    main()
